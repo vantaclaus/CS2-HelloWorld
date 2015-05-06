c8b6d869e059d72f0251fa3e93e3baf74390c2f6
vantaclaus <myersjac070@gmail.com>
2015-05-06 08:54:53 -0700

    Added Scanner

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,11 @@
+import java.util.*;
 public class HelloWorld {
+  private static Scanner in = new Scanner(System.in);
+
   public static void main(String[] blargs){
     System.out.println("Hello World!");
     System.out.println("Blep");
+    String s = in.nextLine();
+    System.out.println(s);
   }
 }
